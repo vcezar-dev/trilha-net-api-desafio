f417ca22a0777b452fa21cf2747b0db455c82f3a
“vcezar-dev” <vcezaroliveiraa@gmail.com>
2025-11-16 22:10:41 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -11,18 +11,9 @@ builder.Services.AddDbContext<OrganizadorContext>(options =>
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
